85935c1ee60a175af803e8558f60200efff1c167
Nicholasky <ka.nikorasu@gmail.com>
2026-08-22 15:45:58 -0500

    Add BoardBuilderOQ + comment idea?

---
+++ src/Game/BoardBuilderOQ.java
@@ -0,0 +1,7 @@
+// Intended to build a board based on pre-defined locations of 4 tiles with purple spheres
+// So, take an argument that says "Which arrangement of purple spheres is this?"
+// and then build a board based on that
+
+public class BoardBuilderOQ {
+
+}
