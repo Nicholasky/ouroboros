e77e3741e821575f8ab03d5740da6f274b53bc82
Nicholasky <ka.nikorasu@gmail.com>
2026-08-22 17:29:05 -0500

    start adding OQ board-building

--- src/Game/BoardBuilderOQ.java
+++ src/Game/BoardBuilderOQ.java
@@ -2,6 +2,18 @@
 // So, take an argument that says "Which arrangement of purple spheres is this?"
 // and then build a board based on that
 
-public class BoardBuilderOQ {
+public class BoardBuilderOQ implements BoardBuilder {
+
+    // by default for now
+    public Board build(){
+        return build(0, 1, 2, 3);
+    }
+
+    // takes 4 different purple positions, build the other tiles around them
+    public Board build(int p1, int p2, int p3, int p4){
+        Board board = new Board();
+
+        return board;
+    }
 
 }
