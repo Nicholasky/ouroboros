38a23a942f8921189498755a452a0287c5791e5e
Nicholasky <ka.nikorasu@gmail.com>
2026-08-22 21:33:42 -0500

    Add tile constructor

--- src/Game/Tile.java
+++ src/Game/Tile.java
@@ -1,6 +1,12 @@
 public class Tile {
     Sphere sp;
     boolean revealed = false;
-    
 
+    public Tile(Sphere sphere){
+        sp = sphere;
+    }
+
+    public Sphere getSphere(){
+        return sp;
+    }
 }
