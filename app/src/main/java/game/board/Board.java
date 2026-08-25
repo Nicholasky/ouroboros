08c06c727e7ac8216adada69b5358b780b0b2bd2
Nicholasky <ka.nikorasu@gmail.com>
2026-08-25 15:02:47 -0500

    add values for Color enum
    maybe this color enum should be removed soon

--- app/src/main/java/game/board/Board.java
+++ app/src/main/java/game/board/Board.java
@@ -22,7 +22,7 @@ public class Board {
         for(int i = 0; i < 5; i++){
             for(int j = 0; j < 5; j++){
                 index = 5*i+j;
-                buildStr[index] = tiles[i][j].getSphere().getType().getColor();
+                buildStr[index] = (char)tiles[i][j].getSphere().getType().getColor().index();   // probably fragile design
             }
         }
 
