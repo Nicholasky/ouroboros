66d8d3bc6e15f5b10a9f718a3f380c9afe832888
Nicholasky <ka.nikorasu@gmail.com>
2026-08-25 14:01:32 -0500

    add a toString for boards to aid in comparison
    Doesnt distinguish hidden and revealed tiles.

--- src/main/game/board/Board.java
+++ src/main/game/board/Board.java
@@ -13,4 +13,21 @@ public class Board {
     }
 
 
+    // Note: No difference in tostring for a hidden vs visible tile 
+    @Override
+    public String toString(){  
+        char[] buildStr = new char[25];
+
+        int index;
+        for(int i = 0; i < 5; i++){
+            for(int j = 0; j < 5; j++){
+                index = 5*i+j;
+                buildStr[index] = tiles[i][j].getSphere().getType().getColor();
+            }
+        }
+
+        String returnStr = new String(buildStr);
+        return returnStr;
+    }
+
 }
