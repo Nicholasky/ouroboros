b1acbaedbcbf01d4031a70cb0c55e112ce1be955
Nicholasky <ka.nikorasu@gmail.com>
2026-08-28 14:10:38 -0500

    Notes, initial goals for OQSolver.

---
+++ app/src/main/java/solver/oq/OQSolver.java
@@ -0,0 +1,22 @@
+package solver.oq;
+
+
+// intends to solve OQ boards / show optimal moves given a board state
+// as the hidden state of the board is unveiled, different pre-solved recommendations are made
+public class OQSolver {
+    
+    // Needs to be able to track all 12650 board-states, to know which are still valid, recommendable, and CALCULABLE states
+
+
+    // Needs conditions to "disqualify" board states quickly, so that after a choice is made,
+    // it is known that board b1, board b2, board b3... are no longer eligible or calculable
+
+
+    // Needs a basis / "value" for applying actions on a board. Expected value of spheres ? 
+
+
+
+
+    
+
+}
