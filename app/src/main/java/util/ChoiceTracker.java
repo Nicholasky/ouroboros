b0ce996b14781efb10768d2432ed4a66aa709ac6
Nicholasky <ka.nikorasu@gmail.com>
2026-08-29 18:28:41 -0500

    add elimination logic with immutability

--- app/src/main/java/util/ChoiceTracker.java
+++ app/src/main/java/util/ChoiceTracker.java
@@ -3,11 +3,29 @@ package util;
 import java.util.BitSet;
 
 public class ChoiceTracker{
-    BitSet choices;
+    private final BitSet choices;
+    private final int numChoices;
+
+    public ChoiceTracker(BitSet choices, int numChoices){
+        this.choices = choices;
+        this.numChoices = numChoices;
+    }
 
     public ChoiceTracker(int numChoices){
-        choices = new BitSet(numChoices);
+        this.choices = new BitSet(numChoices);
+        this.numChoices = numChoices;
     }
-    
+
+    public ChoiceTracker eliminate(int[] indices){
+        BitSet next = (BitSet)choices.clone();
+
+        // the alternative was to create BitSets outside of this class with indices false and AND them together...
+        // but it was not my desire to add BitSet everywhere around the code
+        for(int i : indices){
+            next.clear(i);
+        }
+        
+        return new ChoiceTracker(next, numChoices);
+    }
 
 }
