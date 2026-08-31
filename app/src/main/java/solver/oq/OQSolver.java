370c3e8865ba95f4508e97472c24efd554e70802
Nicholasky <ka.nikorasu@gmail.com>
2026-08-31 17:33:11 -0500

    moving comments + start writing the solution
    ! Not runnable

--- app/src/main/java/solver/oq/OQSolver.java
+++ app/src/main/java/solver/oq/OQSolver.java
@@ -3,19 +3,27 @@ package solver.oq;
 
 // intends to solve OQ boards / show optimal moves given a board state
 // as the hidden state of the board is unveiled, different pre-solved recommendations are made
+
+// Needs to be able to track all 12650 board-states, to know which are still valid, recommendable, and CALCULABLE states
+
+// Needs conditions to "disqualify" board states quickly, so that after a choice is made,
+// it is known that board b1, board b2, board b3... are no longer eligible or calculable
+
+// Needs a basis / "value" for applying actions on a board. Expected value of spheres ? 
+
 public class OQSolver {
+    private static final int NUM_MOVES = 7;
     
-    // Needs to be able to track all 12650 board-states, to know which are still valid, recommendable, and CALCULABLE states
+    public int solve(){
+        return solveRecur(state, remainingboards, movesleft);
+
+    }
 
 
-    // Needs conditions to "disqualify" board states quickly, so that after a choice is made,
-    // it is known that board b1, board b2, board b3... are no longer eligible or calculable
+    private int solveRecur(??? state, BitSet remainingboards, int movesLeft){
+        
 
-
-    // Needs a basis / "value" for applying actions on a board. Expected value of spheres ? 
-
-
-
+    }
 
     
 
