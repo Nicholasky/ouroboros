497358bc7eec7c8ae551bd48ed7c0113f58c7376
Nicholasky <ka.nikorasu@gmail.com>
2026-09-02 17:24:02 -0500

    Start implementing OQ Solver
    (Big WIP)

--- app/src/main/java/solver/oq/OQSolver.java
+++ app/src/main/java/solver/oq/OQSolver.java
@@ -1,5 +1,6 @@
 package solver.oq;
 
+import java.util.BitSet;
 
 // intends to solve OQ boards / show optimal moves given a board state
 // as the hidden state of the board is unveiled, different pre-solved recommendations are made
@@ -15,28 +16,42 @@ public class OQSolver {
     private static final int NUM_MOVES = 7;
     
     public int solve(){
-        return solveRecur(state, remainingboards, movesleft);
+        BitSet bits = new BitSet(12650);
+        bits.set(0, 12650);
+        return solveRecur(bits, NUM_MOVES);
         
     }
 
 
-    private int solveRecur(??? state, BitSet remainingboards, int movesLeft){
+    // Problem:
+    // it is unknown which board is being played;
+    // the "board" makes a choice in reveal information every turn.
+
+    // idea to solve:
+    // recurse on "expected value" of boards
+    // so that the unknown "board choice" is accounted for,
+    // together with teh probability that choice occurs
+
+    // BitSet b = OQTruthTable.getMatchingBoards(d, h) 
+    // P(tile d has heat h) = b.cardinality / remainingBoards.cardinality  
+
+    // Recursive call would be: (For all tile-heat combinations, get maximum of): 
+    //      (b.cardinality / remainingBoards.cardinality) * solveRecur(state, b, movesLeft
+    private int solveRecur(BitSet remainingBoards, int movesLeft){
+        int val = 0;
+
+        // cardinality = 0 should only be possible if an invalid move occurs
+        if(remainingBoards.cardinality() == 0)
+            return 0;
         
+        if(movesLeft == 0){
+            return 0;
+        }
 
-        // Problem:
-        // it is unknown which board is being played;
-        // the "board" makes a choice in reveal information every turn.
 
-        // idea to solve:
-        // recurse on "expected value" of boards
-        // so that the unknown "board choice" is accounted for,
-        // together with teh probability that choice occurs
 
-        // BitSet b = OQTruthTable.getMatchingBoards(d, h) 
-        // P(tile d has heat h) = b.cardinality / remainingBoards.cardinality  
 
-        // Recursive call would be: (For all tile-heat combinations, get maximum of): 
-        //      (b.cardinality / remainingBoards.cardinality) * solveRecur(state, b, movesLeft)
+        return val; // TEMP 
 
     }
 
